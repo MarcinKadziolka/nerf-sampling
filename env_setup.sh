419a596cf70ef69b25e8ed380b60117ce596cb90
Joanna Waczyńska <joanna.waczynska@orange.com>
2023-08-02 22:54:45 +0200

    add last commit from nerf-pytorch

--- env_setup.sh
+++ env_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd sphere_nerf_mod
 git clone https://github.com/yenchenlin/nerf-pytorch.git
+git checkout 63a5a630c9abd62b0f21c08703d0ac2ea7d4b9dd
 mv nerf-pytorch nerf_pytorch
 cd nerf_pytorch
 touch __init__.py
