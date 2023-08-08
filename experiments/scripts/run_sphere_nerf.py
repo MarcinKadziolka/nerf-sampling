2e9a0038ce555e685827acd4ce0c21c7b96cd53a
Joanna Waczyńska <joanna.waczynska@orange.com>
2023-08-08 16:29:06 +0200

    .

--- experiments/scripts/run_sphere_nerf.py
+++ experiments/scripts/run_sphere_nerf.py
@@ -1,24 +1,49 @@
 """Script for run sphere nerf."""
 import click
 import torch
+import yaml
 
 from nerf_pytorch.run_nerf import train
+
+torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
 
 @click.command()
 @click.option(
-    "--model",
-    help="Name of selected model",
+    "--hparams_path",
+    help="Type of selected dataset",
     type=str,
-    default="nerf"
+    default="experiments/configs/normalized.yaml"
+)
+@click.option(
+    "--model",
+    help="Selected model",
+    type=str,
+    default="airplane"
 )
 def main(
+    hparams_path: str,
     model: str,
 ):
     """Main."""
-    if model == "nerf":
-        torch.set_default_tensor_type('torch.cuda.FloatTensor')
-        train()
+    with open(hparams_path, "r") as fin:
+        hparams = yaml.safe_load(fin)[model]
+
+    train(
+        half_res=hparams["white_bkgd"],
+        no_batching=hparams["no_batching"],
+        N_samples=hparams["N_samples"],
+        N_importance=hparams["N_importance"],
+        use_viewdirs=hparams["use_viewdirs"],
+        white_bkgd=hparams["white_bkgd"],
+        device=hparams["device"],
+        N_rand=hparams["N_rand"],
+        expname=hparams["data"]["expname"],
+        basedir=hparams["data"]["basedir"],
+        datadir=hparams["data"]["datadir"],
+        dataset_type=hparams["data"]["dataset_type"],
+        config_path=hparams_path
+    )
 
 
 if __name__ == "__main__":
