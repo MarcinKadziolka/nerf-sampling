1bb564187ad738fa9a8fe9f21f0f899bb9d2f1c5
ktylus <karaxek23@gmail.com>
2023-08-13 19:56:30 +0200

    fixed quadratic equation test

--- sphere_nerf_mod/tests/test_utils.py
+++ sphere_nerf_mod/tests/test_utils.py
@@ -1,13 +1,30 @@
 """Tests for all utils module functions."""
+import torch
 
 from sphere_nerf_mod.utils import solve_quadratic_equation
 
 
 def test_solve_quadratic_equation():
     """Test solving quadratic equation."""
-    assert solve_quadratic_equation(1, 2, 1) == [-1]
-    assert solve_quadratic_equation(1, 1, 1) == []
-    assert solve_quadratic_equation(1, 7, 6) == [-6, -1]
+    assert torch.equal(
+        solve_quadratic_equation(
+            torch.Tensor([1]),
+            torch.Tensor([2]),
+            torch.Tensor([1])
+        ),
+        torch.Tensor([[-1], [torch.nan]])
+    )
+    assert torch.equal(
+        solve_quadratic_equation(
+            torch.Tensor([1, 4, 5]),
+            torch.Tensor([1, 4, 6]),
+            torch.Tensor([1, 1, 1])
+        ),
+        torch.Tensor(
+            [[torch.nan, -0.5, -1],
+            [torch.nan, torch.nan, -0.2]]
+        )
+    )
 
 
 if __name__ == "__main__":
