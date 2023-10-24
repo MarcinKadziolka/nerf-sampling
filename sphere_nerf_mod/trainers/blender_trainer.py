137102823430a6aeee1fef75dfd6f24dfa96f595
waczyjoan <joanna@joanna-Lenovo-Legion-7-15IMH05>
2023-10-24 17:09:50 +0200

    come back to great results

--- sphere_nerf_mod/trainers/blender_trainer.py
+++ sphere_nerf_mod/trainers/blender_trainer.py
@@ -102,18 +102,18 @@ class SphereBlenderTrainer(Blender.BlenderTrainer):
         #    base_camera=self.c2w
         #)
 
-        reflected_rays = Lines(
-            torch.zeros_like(rays_origins),
-            -rays_directions
-        )
+        #reflected_rays = Lines(
+        #    torch.zeros_like(rays_origins),
+        #    -rays_directions
+        #)
 
         # Sample points on spheres and transform them into a
         # single number representation
         sphere_nerf_points = self.sample_points_on_spheres(
-            reflected_rays
+            rays
         ).swapaxes(0, 1)  # [n_rays, m_spheres/n_points, 3]
 
-        z_sphere = reflected_rays.transform_points_to_single_number_representation(
+        z_sphere = rays.transform_points_to_single_number_representation(
             sphere_nerf_points
         )
 
@@ -156,6 +156,10 @@ class SphereBlenderTrainer(Blender.BlenderTrainer):
             pytest=pytest
         )
 
+        a = rgb_map.all()
+        b = disp_map.all()
+        c = acc_map.all()
+
         return None, None, None, rgb_map, disp_map, acc_map, raw
 
     def sample_points_on_spheres(
@@ -196,97 +200,3 @@ class SphereBlenderTrainer(Blender.BlenderTrainer):
 
     def create_nerf_model(self):
         return self._create_nerf_model(model=SphereMoreViewsNeRFV2)
-
-    def raw2outputs(
-        self,
-        raw: torch.Tensor,
-        z_vals: torch.Tensor,
-        rays_d: torch.Tensor,
-        raw_noise_std: float = 0,
-        white_bkgd: bool = False,
-        pytest: bool = False
-    ) -> Tuple[
-        torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor
-    ]:
-        """Transforms model's predictions to semantically meaningful values.
-
-        Args:
-            raw (torch.Tensor):
-            Model's prediction, shape [num_rays, num_samples along ray, 4].
-            z_vals (torch.Tensor):
-            Integration time, shape [num_rays, num_samples along ray].
-            rays_d (torch.Tensor):
-            Direction of each ray, shape [num_rays, 3].
-            raw_noise_std (float, optional):
-            Standard deviation for raw noise (default: 0).
-            white_bkgd (bool, optional):
-            Flag indicating white background (default: False).
-            pytest (bool, optional):
-            Flag indicating whether this is a pytest run (default: False).
-
-        Returns:
-            Tuple of torch.Tensors:
-            - rgb_map (torch.Tensor):
-                Estimated RGB color of a ray, shape [num_rays, 3].
-            - disp_map (torch.Tensor):
-                Disparity map, the inverse of the depth map, shape [num_rays].
-            - acc_map (torch.Tensor):
-                Sum of weights along each ray, shape [num_rays].
-            - weights (torch.Tensor):
-                Weights assigned to each sampled color,
-                shape [num_rays, num_samples].
-            - depth_map (torch.Tensor):
-                Estimated distance to object, shape [num_rays].
-        """
-        raw2alpha = lambda raw, dists, act_fn=F.relu: 1. - torch.exp(-act_fn(raw) * dists)
-
-        # Compute distances between z values and scale by ray direction
-        dists = z_vals[..., 1:] - z_vals[..., :-1]
-        dists = torch.cat(
-            [dists, torch.Tensor([1e10]).expand(dists[..., :1].shape)], -1
-        )  # [num_rays, num_samples]
-        dists = dists * torch.norm(rays_d[..., None, :], dim=-1)
-
-        # Apply sigmoid to the first 3 values of raw to get RGB
-        rgb = torch.sigmoid(raw[..., :3])  # [num_rays, num_samples, 3]
-
-        # Generate noise based on raw_noise_std if applicable
-        noise = 0.
-        if raw_noise_std > 0.:
-            noise = torch.randn(raw[..., 3].shape) * raw_noise_std
-
-            # Overwrite randomly sampled data if in pytest mode
-            if pytest:
-                np.random.seed(0)
-                noise = np.random.rand(*list(raw[..., 3].shape)) * raw_noise_std
-                noise = torch.Tensor(noise)
-
-        # Calculate alpha values using raw2alpha function
-        alpha = raw2alpha(raw[..., 3] + noise, dists)  # [num_rays, num_samples]
-
-        # Compute weights using cumulative product
-        weights = alpha * torch.cumprod(torch.cat(
-            [torch.ones((alpha.shape[0], 1)), 1. - alpha + 1e-10], -1), -1)[:, :-1]
-
-        # Compute the final RGB map
-        rgb_map = torch.sum(weights[..., None] * rgb, -2)  # [num_rays, 3]
-        #n_spheres = raw.shape[1]
-        #rgb_map = torch.sum(raw[..., 3, None] * rgb, -1) / n_spheres
-        #aa = rgb_map.max()
-        #bb
-        #rgb_map = torch.where(rgb_map < 0, 0, rgb_map)
-
-        # Compute the depth map and disparity map
-        depth_map = torch.sum(weights * z_vals, -1)
-        disp_map = 1. / torch.max(
-            1e-10 * torch.ones_like(depth_map), depth_map / torch.sum(weights, -1)
-        )
-
-        # Compute the accumulation map
-        acc_map = torch.sum(weights, -1)
-
-        # If using a white background, adjust the RGB map
-        if white_bkgd:
-            rgb_map = rgb_map + (1. - acc_map[..., None])
-
-        return rgb_map, disp_map, acc_map, weights, depth_map
