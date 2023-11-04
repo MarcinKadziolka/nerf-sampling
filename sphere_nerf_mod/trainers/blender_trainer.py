030cd4d7406158132210072f5c8a307513dcc9c0
ktylus <karaxek23@gmail.com>
2023-11-04 18:28:06 +0100

    using rays' directions for determining image position

--- sphere_nerf_mod/trainers/blender_trainer.py
+++ sphere_nerf_mod/trainers/blender_trainer.py
@@ -16,7 +16,6 @@ from sphere_nerf_mod.models import (
 )
 
 from sphere_nerf_mod.utils import change_cartesian_to_spherical
-from sphere_nerf_mod.utils import reflected
 
 
 class SphereBlenderTrainer(Blender.BlenderTrainer):
@@ -95,17 +94,11 @@ class SphereBlenderTrainer(Blender.BlenderTrainer):
         rays_directions = rays_d
         rays = Lines(rays_origins, rays_directions)
 
-        #rays_o_reflected, rays_d_reflected = reflected(
-        #    spheres=self.spheres,
-        #    img_height=self.H,
-        #    img_width=self.W,
-        #    base_camera=self.c2w
-        #)
-
-        #reflected_rays = Lines(
-        #    torch.zeros_like(rays_origins),
-        #    -rays_directions
-        #)
+        reflected_rays = Lines(
+            torch.zeros_like(rays_origins),
+            F.normalize(rays_origins + rays_directions, dim=-1)
+        )
+        rays = reflected_rays
 
         # Sample points on spheres and transform them into a
         # single number representation
@@ -156,10 +149,6 @@ class SphereBlenderTrainer(Blender.BlenderTrainer):
             pytest=pytest
         )
 
-        a = rgb_map.all()
-        b = disp_map.all()
-        c = acc_map.all()
-
         return None, None, None, rgb_map, disp_map, acc_map, raw
 
     def sample_points_on_spheres(
@@ -200,3 +189,46 @@ class SphereBlenderTrainer(Blender.BlenderTrainer):
 
     def create_nerf_model(self):
         return self._create_nerf_model(model=SphereMoreViewsNeRFV2)
+
+    def raw2outputs(self, raw, z_vals, rays_d, raw_noise_std=0, white_bkgd=False, pytest=False):
+        """Transforms model's predictions to semantically meaningful values.
+        Args:
+            raw: [num_rays, num_samples along ray, 4]. Prediction from model.
+            z_vals: [num_rays, num_samples along ray]. Integration time.
+            rays_d: [num_rays, 3]. Direction of each ray.
+        Returns:
+            rgb_map: [num_rays, 3]. Estimated RGB color of a ray.
+            disp_map: [num_rays]. Disparity map. Inverse of depth map.
+            acc_map: [num_rays]. Sum of weights along each ray.
+            weights: [num_rays, num_samples]. Weights assigned to each sampled color.
+            depth_map: [num_rays]. Estimated distance to object.
+        """
+        raw2alpha = lambda raw, dists, act_fn=F.relu: 1. - torch.exp(-act_fn(raw) * dists)
+        dists = z_vals[..., 1:] - z_vals[..., :-1]
+        dists = torch.cat([dists, torch.Tensor([1e10]).expand(dists[..., :1].shape)], -1)  # [N_rays, N_samples]
+        dists = dists * torch.norm(rays_d[..., None, :], dim=-1)
+        rgb = torch.sigmoid(raw[..., :3])  # [N_rays, N_samples, 3]
+        noise = 0.
+        if raw_noise_std > 0.:
+            noise = torch.randn(raw[..., 3].shape) * raw_noise_std
+            # Overwrite randomly sampled data if pytest
+            if pytest:
+                np.random.seed(0)
+                noise = np.random.rand(*list(raw[..., 3].shape)) * raw_noise_std
+                noise = torch.Tensor(noise)
+        alpha = raw2alpha(raw[..., 3] + noise, dists)  # [N_rays, N_samples]
+        weights = alpha * torch.cumprod(torch.cat(
+            [torch.ones((alpha.shape[0], 1)), 1. - alpha + 1e-10], -1), -1)[:, :-1]
+
+        n_spheres = raw.shape[1]
+        #rgb_map = torch.sum(weights[..., None] * rgb, -2)  # [N_rays, 3]
+        rgb_map = torch.sum(raw[..., 3, None] * rgb, -1) / n_spheres
+        # rgb_map = torch.sum(rgb, -1) / n_spheres
+        rgb_map = torch.where(rgb_map < 0, 0, rgb_map)
+
+        depth_map = torch.sum(weights * z_vals, -1)
+        disp_map = 1. / torch.max(1e-10 * torch.ones_like(depth_map), depth_map / torch.sum(weights, -1))
+        acc_map = torch.sum(weights, -1)
+        if white_bkgd:
+            rgb_map = rgb_map + (1. - acc_map[..., None])
+        return rgb_map, disp_map, acc_map, weights, depth_map
