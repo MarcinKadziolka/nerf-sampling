e62e981a6057a3dcfe06b8ffb15ead7dd45f2419
Mateusz Rajski <mateusz.rajski@swmansion.com>
2024-01-19 13:06:54 +0100

    Track alphas

--- sphere_nerf_mod/trainers/sampling_trainer.py
+++ sphere_nerf_mod/trainers/sampling_trainer.py
@@ -4,6 +4,8 @@ from nerf_pytorch.nerf_utils import NeRF, create_nerf
 from sphere_nerf_mod.samplers.baseline_sampler import BaselineSampler
 from safetensors.torch import save_file
 import os
+import torch.nn.functional as F
+import numpy as np
 
 class SamplingTrainer(Blender.BlenderTrainer):
     """Trainer for blender data."""
@@ -63,7 +65,7 @@ class SamplingTrainer(Blender.BlenderTrainer):
 
         return optimizer, render_kwargs_train, render_kwargs_test
     
-    def save_rays_data(self, rays_o, pts):
+    def save_rays_data(self, rays_o, pts, alpha):
         """
         Saves rays data for later visualization
         """
@@ -76,6 +78,7 @@ class SamplingTrainer(Blender.BlenderTrainer):
         tensors = {
             'origins': rays_o.contiguous(),
             'pts': pts.contiguous(),
+            'alpha': alpha.contiguous(),
         }
 
         save_file(tensors, filename)
@@ -108,11 +111,59 @@ class SamplingTrainer(Blender.BlenderTrainer):
 
             pts, z_vals = sampling_network.forward(rays_o, rays_d)
 
-            self.save_rays_data(rays_o, pts)
 
             raw = network_query_fn(pts, viewdirs, network_fn)
-            rgb_map, disp_map, acc_map, weights, depth_map = self.raw2outputs(
+            rgb_map, disp_map, acc_map, weights, depth_map, alpha = self.raw2outputs(
                 raw, z_vals, rays_d, raw_noise_std, white_bkgd,
                 pytest=pytest
             )
+
+            self.save_rays_data(rays_o, pts, alpha)
+
         return rgb_map, disp_map, acc_map, weights, depth_map, z_vals, weights, raw
+    
+    def raw2outputs(self, raw, z_vals, rays_d, raw_noise_std=0, white_bkgd=False, pytest=False, **kwargs):
+        """Transforms model's predictions to semantically meaningful values.
+        Args:
+            raw: [num_rays, num_samples along ray, 4]. Prediction from model.
+            z_vals: [num_rays, num_samples along ray]. Integration time.
+            rays_d: [num_rays, 3]. Direction of each ray.
+        Returns:
+            rgb_map: [num_rays, 3]. Estimated RGB color of a ray.
+            disp_map: [num_rays]. Disparity map. Inverse of depth map.
+            acc_map: [num_rays]. Sum of weights along each ray.
+            weights: [num_rays, num_samples]. Weights assigned to each sampled color.
+            depth_map: [num_rays]. Estimated distance to object.
+        """
+        raw2alpha = lambda raw, dists, act_fn=F.relu: 1. - torch.exp(-act_fn(raw) * dists)
+
+        dists = z_vals[..., 1:] - z_vals[..., :-1]
+        dists = torch.cat([dists, torch.Tensor([1e10]).expand(dists[..., :1].shape)], -1)  # [N_rays, N_samples]
+
+        dists = dists * torch.norm(rays_d[..., None, :], dim=-1)
+
+        rgb = torch.sigmoid(raw[..., :3])  # [N_rays, N_samples, 3]
+        noise = 0.
+        if raw_noise_std > 0.:
+            noise = torch.randn(raw[..., 3].shape) * raw_noise_std
+
+            # Overwrite randomly sampled data if pytest
+            if pytest:
+                np.random.seed(0)
+                noise = np.random.rand(*list(raw[..., 3].shape)) * raw_noise_std
+                noise = torch.Tensor(noise)
+
+        alpha = raw2alpha(raw[..., 3] + noise, dists)  # [N_rays, N_samples]
+        # weights = alpha * tf.math.cumprod(1.-alpha + 1e-10, -1, exclusive=True)
+        weights = alpha * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1)), 1. - alpha + 1e-10], -1), -1)[:,
+                          :-1]
+        rgb_map = torch.sum(weights[..., None] * rgb, -2)  # [N_rays, 3]
+
+        depth_map = torch.sum(weights * z_vals, -1)
+        disp_map = 1. / torch.max(1e-10 * torch.ones_like(depth_map), depth_map / torch.sum(weights, -1))
+        acc_map = torch.sum(weights, -1)
+
+        if white_bkgd:
+            rgb_map = rgb_map + (1. - acc_map[..., None])
+
+        return rgb_map, disp_map, acc_map, weights, depth_map, alpha
