f1a09580d95eec4646e54eb69a6fc9e0064a5c21
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2024-04-02 21:15:49 +0200

    Delete files used in previous structuring of the project
    Delete setup - this project will no longer be installable package
    Delete requirements - unknown usage of provided package

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="sphere_nerf_mod",
-    version="0.0.1",
-    description="Nerf with coordinates located on the spheres",
-    author="ktylus, waczyjoan",
-    packages=find_packages(),
-)
