3c8511f5f7a233c5fbbd8d83981d0ff87a473be5
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2024-05-03 23:53:36 +0200

    Start initializing sampling network architecture using list of hidden sizes
    Easily modifiable architecture.
    Depth and width are now a configurable hyperparameter.

--- nerf_sampling/samplers/baseline_sampler.py
+++ nerf_sampling/samplers/baseline_sampler.py
@@ -12,6 +12,8 @@ class BaselineSampler(nn.Module):
 
     def __init__(
         self,
+        hidden_sizes: list[int] = [128 for _ in range(6)],
+        cat_hidden_sizes: list[int] = [128, 128, 128, 128, 256],
         origin_channels: int = 3,
         direction_channels: int = 3,
         near: int = 2,
@@ -21,6 +23,9 @@ class BaselineSampler(nn.Module):
         """Initializes sampling network.
 
         Args:
+            hidden_sizes: Hidden size of each consecutive linear layer for origin and direction.
+            cat_hidden_sizes: Hidden size of each consecutive linear layer
+                for concatenated output of origin and direction layers.
             origin_channels: Expected number of channels of ray origin vector.
             direction_channels: Expected number of channels of ray direction vector.
             near: Nearest distance for a ray.
@@ -28,10 +33,6 @@ class BaselineSampler(nn.Module):
             n_samples: Number of samples to output along a ray.
         """
         super(BaselineSampler, self).__init__()
-        self.w1 = 256
-        self.w2 = 128
-        self.origin_channels = origin_channels
-        self.direction_channels = direction_channels
         self.n_samples = n_samples
         self.far = far
         self.near = near
@@ -43,39 +44,39 @@ class BaselineSampler(nn.Module):
             multires=10, input_dims=direction_channels
         )
 
-        self.origin_layers = nn.ModuleList(
-            [
-                nn.Linear(self.origin_dims, self.w2),
-                nn.Linear(self.w2, self.w2),
-                nn.Linear(self.w2, self.w2),
-                nn.Linear(self.w2, self.w2),
-                nn.Linear(self.w2, self.w2),
-                nn.Linear(self.w2, self.w2),
-            ]
-        )
+        origin_layers: list[nn.Linear | nn.ReLU] = [
+            nn.Linear(self.origin_dims, hidden_sizes[0]),
+            nn.ReLU(),
+        ]
+        direction_layers: list[nn.Linear | nn.ReLU] = [
+            nn.Linear(self.direction_dims, hidden_sizes[0]),
+            nn.ReLU(),
+        ]
 
-        self.direction_layers = nn.ModuleList(
-            [
-                nn.Linear(self.direction_dims, self.w2),
-                nn.Linear(self.w2, self.w2),
-                nn.Linear(self.w2, self.w2),
-                nn.Linear(self.w2, self.w2),
-                nn.Linear(self.w2, self.w2),
-                nn.Linear(self.w2, self.w2),
-            ]
-        )
+        for i, size in enumerate(hidden_sizes[:-1]):
+            for layers in [origin_layers, direction_layers]:
+                layers.append(
+                    nn.Linear(in_features=size, out_features=hidden_sizes[i + 1])
+                )
+                layers.append(nn.ReLU())
 
-        self.layers = nn.ModuleList(
-            [
-                nn.Linear(self.w2 * 2, self.w2),
-                nn.Linear(self.w2, self.w2),
-                nn.Linear(self.w2, self.w2),
-                nn.Linear(self.w2, self.w2),
-                nn.Linear(self.w2, self.w1),
-            ]
-        )
+        cat_layers: list[nn.Linear | nn.ReLU | nn.Sigmoid] = [
+            nn.Linear(hidden_sizes[-1] * 2, cat_hidden_sizes[0]),
+            nn.ReLU(),
+        ]
 
-        self.last = nn.Linear(self.w1, self.n_samples)
+        for i, size in enumerate(cat_hidden_sizes[:-1]):
+            cat_layers.append(
+                nn.Linear(in_features=size, out_features=cat_hidden_sizes[i + 1])
+            )
+            cat_layers.append(nn.ReLU())
+
+        self.origin_layers = nn.Sequential(*origin_layers)
+        self.direction_layers = nn.Sequential(*direction_layers)
+        self.cat_layers = nn.Sequential(*cat_layers)
+
+        self.to_n_samples = nn.Linear(cat_hidden_sizes[-1], self.n_samples)
+        self.sigmoid = nn.Sigmoid()
 
     def scale_to_near_far(self, outputs, rays_o, rays_d):
         """Directly scales points from NN output to the range [NEAR, FAR]."""
@@ -98,24 +99,12 @@ class BaselineSampler(nn.Module):
         embedded_origin = self.origin_embedder(rays_o)
         embedded_direction = self.direction_embedder(rays_d)
 
-        origin_outputs = embedded_origin
-        direction_outputs = embedded_direction
-
-        for layer in self.origin_layers:
-            origin_outputs = layer(origin_outputs)
-            origin_outputs = F.relu(origin_outputs)
-
-        for layer in self.direction_layers:
-            direction_outputs = layer(direction_outputs)
-            direction_outputs = F.relu(direction_outputs)
+        origin_outputs = self.origin_layers(embedded_origin)
+        direction_outputs = self.direction_layers(embedded_direction)
 
         outputs = torch.cat([origin_outputs, direction_outputs], -1)
 
-        for layer in self.layers:
-            outputs = layer(outputs)
-            outputs = F.relu(outputs)
-
-        outputs = self.last(outputs)
-        outputs = F.sigmoid(outputs)
-
-        return self.scale_to_near_far(outputs, rays_o, rays_d)
+        concat_outputs = self.cat_layers(outputs)
+        n_samples_output = self.to_n_samples(concat_outputs)
+        final_outputs = self.sigmoid(n_samples_output)
+        return self.scale_to_near_far(final_outputs, rays_o, rays_d)
