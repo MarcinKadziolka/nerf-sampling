07f12c69db3e512d7d3c3134da3286f34db9b54f
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2024-05-03 16:42:43 +0200

    Add mock training function to test sampler network
    According to this test sampler network is correctly implemented,
    as it successfully places samples in the regions
    of the highest density.

---
+++ nerf_sampling/tests/test_sampling_network.py
@@ -0,0 +1,57 @@
+from nerf_sampling.samplers.baseline_sampler import BaselineSampler
+import matplotlib.pyplot as plt
+import torch
+from nerf_sampling.nerf_pytorch import visualize
+
+
+def calculate_densities(pts, target_center, gaussian_width):
+    """Differentiable density distribution around targets."""
+    densities = torch.exp(-0.5 * ((pts[:, 0] - target_center) ** 2) / gaussian_width**2)
+    return densities
+
+
+def train(plot, final_plot):
+    """Mock train function to test sampler network."""
+    target_centers = [2, 3, 4, 5, 6]
+    gaussian_width = 0.5
+    sampling_network = BaselineSampler()
+    rays_o = torch.zeros(len(target_centers), 3)
+    for i, ray in enumerate(rays_o):
+        ray[2] += i
+    rays_d = torch.Tensor([[1, 0, 0]]).repeat(len(target_centers), 1)
+    optim = torch.optim.Adam(sampling_network.parameters())
+
+    for i in range(500):
+        pts, _ = sampling_network(rays_o, rays_d)
+        loss = 0
+        for pt, target_center in zip(pts, target_centers):
+            densities = calculate_densities(pt, target_center, gaussian_width)
+            loss -= torch.mean(densities)
+
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        if i % 10 == 0:
+            print(
+                f"Step {i}, Loss: {loss.item()}, mean pts = {torch.mean(pts, dim=1)}, target = {target_centers}"
+            )
+            if plot:
+                for pt in pts:
+                    visualize.visualize_rays_pts(rays_o, rays_d, pts)
+                visualize.plot_histogram(densities)
+
+    if final_plot:
+        pts, _ = sampling_network(rays_o, rays_d)
+        for pt, target_center in zip(pts, target_centers):
+            densities = calculate_densities(pt, target_center, gaussian_width)
+        visualize.visualize_rays_pts(
+            rays_o, rays_d, pts.cpu().detach(), title=f"{target_centers=}"
+        )
+        visualize.plot_histogram(densities.cpu().detach())
+        plt.show()
+
+
+if __name__ == "__main__":
+    plot = False
+    final_plot = True
+    train(plot, final_plot)
