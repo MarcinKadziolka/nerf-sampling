81621c41676d529c7fcc7a1cdc17a6a9d57bd8e2
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2024-05-03 15:14:56 +0200

    Start logging experiments to wandb dir
    Update cli parameters.
    Use override to update kwargs.

--- nerf_sampling/experiments/scripts/run_density_in_cost.py
+++ nerf_sampling/experiments/scripts/run_density_in_cost.py
@@ -5,62 +5,74 @@ import torch
 import wandb
 import yaml
 
-from nerf_sampling.nerf_pytorch.utils import load_obj_from_config
+from nerf_sampling.nerf_pytorch.utils import load_obj_from_config, override_config
 
 
 @click.command()
 @click.option(
-    "--hparams_path",
+    "--config",
     help="Path to configuration file.",
     type=str,
     default="experiments/configs/lego.yaml",
+    show_default=True,
 )
-@click.option("--model", help="Model type.", type=str, default="lego_sampler_module")
 @click.option(
-    "--wandb_mode",
+    "--dataset",
+    help="Path to dataset folder.",
+    type=str,
+    default="./dataset/lego",
+    show_default=True,
+)
+@click.option(
+    "--model",
+    help="Model type.",
+    type=str,
+    default="lego_sampler_module",
+    show_default=True,
+)
+@click.option(
+    "--wandb",
     type=click.Choice(["online", "offline", "disabled"], case_sensitive=False),
     default="disabled",
     help="Set the mode for wandb logging.",
+    show_default=True,
 )
-def main(
-    hparams_path: str,
-    wandb_mode: Literal["online", "offline", "disabled"],
-    model: str,
-):
+def main(**click_kwargs):
     """Run NeRF and sampling network training with provided configuration."""
-    with open(hparams_path, "r") as fin:
-        hparams = yaml.safe_load(fin)[model]
+    with open(click_kwargs["config"], "r") as fin:
+        model = click_kwargs["model"]
+        config = yaml.safe_load(fin)[model]
 
     torch.manual_seed(42)  # 0
 
     # get names of environment variables
-    datadir = os.environ.get("DATADIR", "./dataset/lego")
-    basedir = os.environ.get("BASEDIR", "./current_logs")
 
-    hparams["kwargs"]["datadir"] = datadir
-    hparams["kwargs"]["basedir"] = basedir
-
-    if hparams["kwargs"]["device"] == "cuda":
+    if config["kwargs"]["device"] == "cuda":
         if torch.cuda.is_available():
             torch.set_default_tensor_type("torch.cuda.FloatTensor")
 
     EPOCHS = 7000000
-    hparams["kwargs"]["density_in_loss"] = True
 
-    samples = 64
-    n_iter = 10
-    density_loss_weight = 1e-3
-    max_density = False
-    hparams["kwargs"]["N_samples"] = samples
-    hparams["kwargs"]["max_density"] = max_density
-    hparams["kwargs"]["sampling_train_frequency"] = n_iter
-    hparams["kwargs"]["density_loss_weight"] = density_loss_weight
-    expname = f"density_in_loss_samples_{samples}_weight_{density_loss_weight}_every_{n_iter}_iter_max_{max_density}"
-    hparams["kwargs"]["expname"] = expname
+    override = {
+        "density_in_loss": True,
+        "max_density": False,
+        "N_samples": 64,
+        "sampling_lr": 1e-4,
+        "density_loss_weight": 1e-3,
+        "sampling_train_frequency": 10,
+    }
+    override_config(config=config["kwargs"], update=override)
 
-    print(f"{wandb_mode=}")
-    wandb.init(project="nerf-sampling", config=hparams["kwargs"], mode=wandb_mode)
-    trainer = load_obj_from_config(cfg=hparams)
+    print(f"wandb: {click_kwargs['wandb']}")
+    wandb.init(
+        project="nerf-sampling", config=config["kwargs"], mode=click_kwargs["wandb"]
+    )
+    basedir = wandb.run.dir
+    datadir = click_kwargs["dataset"]
+    config["kwargs"]["datadir"] = datadir
+    config["kwargs"]["basedir"] = basedir
+
+    trainer = load_obj_from_config(cfg=config)
     trainer.train(N_iters=EPOCHS + 1)
 
     return
