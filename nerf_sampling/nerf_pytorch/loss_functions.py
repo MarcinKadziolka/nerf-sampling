1ed4a6bc76a5f244f40fcac9faaaa7bcab57f975
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2024-05-14 13:45:42 +0200

    Reword documentation in loss function

--- nerf_sampling/nerf_pytorch/loss_functions.py
+++ nerf_sampling/nerf_pytorch/loss_functions.py
@@ -1,10 +1,16 @@
 """Loss functions for sampler network."""
 
 import torch
+    return 1 - torch.mean(alphas_or_weights)
 
 
 def mean_density_loss(density: torch.Tensor) -> torch.Tensor:
-    """Calculate mean density."""
+    """Calculate mean density.
+
+    Because we want to maximize density,
+    returned value has minus sign,
+    so that loss can be minimized.
+    """
     return -torch.mean(density)
 
 
